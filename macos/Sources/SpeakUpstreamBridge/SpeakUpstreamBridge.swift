70ec5c228c41bb6bf35be8cd8d773337e07bf61e
steven gonsalvez <steven.gonsalvez@gmail.com>
2026-09-22 12:43:42 +0100

    feat: add Speak upstream bridge

---
+++ macos/Sources/SpeakUpstreamBridge/SpeakUpstreamBridge.swift
@@ -0,0 +1,8 @@
+@_exported import SpeakAutomationKit
+@_exported import SpeakCore
+@_exported import SpeakHotKeys
+
+/// Pinned upstream packages used as Sayso's shared domain and automation base.
+public enum SpeakUpstreamBridge {
+    public static let revision = "0f0e92028a249e17b58349da3ee8033c73ffd219"
+}
